3789c6e8724e88234502b6d4b2b4e1eaa972d24a
Kelly Stoops <kstoops815@gmail.com>
2018-02-10 14:51:25 -0600

    working on number logic

--- RomanNumerals.App/NumToRoman.cs
+++ RomanNumerals.App/NumToRoman.cs
@@ -1,9 +1,27 @@
-﻿namespace RomanNumerals.Tests
+﻿namespace RomanNumerals.App
 {
     public class NumToRoman
     {
-        public NumToRoman()
+        string result;
+        public string Convert(int number)
         {
+            if(number >= 1)
+            {
+                result =  "I" + Convert(number - 1);
+            }
+            if(number == 4)
+            {
+                result = "IV";
+            }
+            if(number == 5)
+            {
+                result = "V";
+            }
+            if(number > 5)
+            {
+                result = "V" + Convert(number - 1);
+            }
+            return result;
         }
     }
 }
--- RomanNumerals.Tests/NumberConverterTests.cs
+++ RomanNumerals.Tests/NumberConverterTests.cs
@@ -14,9 +14,77 @@ namespace RomanNumerals.Tests
             var numToRoman = new NumToRoman();
 
             //Act
+            var result = numToRoman.Convert(number);
 
             //Assert
+            Assert.AreEqual("I", result);
         }
 
+        [Test]
+        public void Entering_the_number_two_returns_the_roman_numeral_equivalent()
+        {
+            //Arrange
+            var number = 2;
+            var numToRoman = new NumToRoman();
+
+            //Act
+            var result = numToRoman.Convert(number);
+
+            //Assert
+            Assert.AreEqual("II", result);
+        }
+
+        [Test]
+        public void Entering_the_number_three_returns_the_roman_numeral_equivalent()
+        {
+            //Arrange
+            var number = 3;
+            var numToRoman = new NumToRoman();
+
+            //Act
+            var result = numToRoman.Convert(number);
+
+            //Assert
+            Assert.AreEqual("III", result);
+        }
+        [Test]
+        public void Entering_the_number_five_returns_the_roman_numeral_equivalent()
+        {
+            //Arrange
+            var number = 5;
+            var numToRoman = new NumToRoman();
+
+            //Act
+            var result = numToRoman.Convert(number);
+
+            //Assert
+            Assert.AreEqual("V", result);
+        }
+        [Test]
+        public void Entering_the_number_four_returns_the_roman_numeral_equivalent()
+        {
+            //Arrange
+            var number = 4;
+            var numToRoman = new NumToRoman();
+
+            //Act
+            var result = numToRoman.Convert(number);
+
+            //Assert
+            Assert.AreEqual("IV", result);
+        }
+        [Test]
+        public void Entering_the_number_eight_returns_the_roman_numeral_equivalent()
+        {
+            //Arrange
+            var number = 8;
+            var numToRoman = new NumToRoman();
+
+            //Act
+            var result = numToRoman.Convert(number);
+
+            //Assert
+            Assert.AreEqual("VIII", result);
+        }
     }
 }
